a45adb3f251df9fb5af3fc6b2104ee5f6f9407f2
Arianna <apr991212@gmail.com>
2026-09-15 18:28:24 -0600

    deploy: friendzy, favicon nuevo, audios robustos

--- src/utils/chat.js
+++
@@ -1,7 +0,0 @@
-// Los uid de Google (Firebase Auth) son alfanuméricos, por lo que "_" es un
-// separador seguro para construir el id determinístico de la conversación.
-export const getConversationId = (uidA, uidB) =>
-  [uidA, uidB].sort().join("_");
-
-export const otherParticipantUid = (conversationId, uid) =>
-  conversationId.split("_").find((p) => p !== uid);
